038ddc5fcf61c31ed10154fda781faec0e209400
Anthony AL Helou <anthony_alhelou@live.com>
2025-12-04 15:34:47 +0200

    Install tailwind CSS and add import eslint rule

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error('Root container missing in index.html')
+}
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
